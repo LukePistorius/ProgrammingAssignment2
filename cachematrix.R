69b4c723a5b4fc2c24840c0626ece142b6d3b1a3
Luke <luke.pistorius@hotmail.com>
2018-12-25 20:12:02 +0200

    assignmet 2 finished R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below 2 functions work together: the first on you enter the matrix
+## it sets a default (NULL) for i --> if it's the first time this
+## matrix has been inversed this will be NULL. 
+## it also creates a list.
+## (after using this before using cachesolve x$get = the matrix)
 
-## Write a short comment describing this function
+## the second function will do a lookup to X$getinverse() if it's NULL then
+## we know it hasn't been cached if there is a value it will return the inverse.
+## it will then get the x matrix and inverse it with the solve function
+## after that it will store it 
+
+# 1) setting the defaults and x
+# 2) save it to the parent environment and not in the functions environment (Caching)
+# 3) creating a list with 4 functions in it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) i <<- solve
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+# it takes the list created from above as an entree
+# checks if i = NULL, if not then it has been calculated before and it needs to come from the cash
+# if it's the first time, it needs to be calculated and saved
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        
+
+                i <- x$getinverse()
+                if(!is.null(i)) {
+                        message("getting cached data")
+                        return(i)
+                }
+                data <- x$get()
+                i <- solve(data, ...)
+                x$setinverse(i)
+                i
+ }
+        
